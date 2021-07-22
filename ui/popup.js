cea2a088dd6b252c78a1737607d244a297f99b65
W <w@proj-shit.dev>
2021-07-23 03:06:09 +0800

    [FEATURE] code review

--- ui/popup.js
+++ ui/popup.js
@@ -73,3 +73,96 @@ DEPENDENCY.wait([{ id: "platform" }])
 	{ id: "platform", src: "/common/platform.js" },
 	{ id: "feature_loader", src: "/feature/feature_loader.js" },
 ].map(DEPENDENCY.load);
+
+DEPENDENCY.wait([{ id: "feature_loader" }])
+	.then(
+		() => {
+			EventListener().listen(
+				top.FEATURE,
+				"listready",
+				() => {
+					_ = async function () {
+						return new Promise(
+							resolve => {
+								top.document.querySelector("#feature-menu-style") || (
+									stylesheet = top.document.createElement("link"),
+									top.document.head.appendChild(stylesheet),
+									stylesheet.id = "feature-menu-style",
+									stylesheet.rel = "stylesheet",
+									stylesheet.href = "/ui/feature-menu.css",
+									stylesheet.onload = () => {
+										resolve();
+									}
+								);
+							}
+						)
+					}()
+						.then(
+							() => {
+								var menu = top.document.querySelector("#feature-menu");
+								for (const feat of FEATURE.list) {
+									addFeatureToMenu(menu, Object.assign({ name: FEATURE.locale.getName(feat.id) }, feat));
+								}
+							}
+						);
+
+					function onFeatureMenuItemClick(e) {
+						var item = e.currentTarget;
+						var prev = menu.querySelector("[active]");
+						if (item == prev) return;
+
+						var ifr = document.querySelector("#feature-iframe")
+						activeLocale = null;
+
+						Promise.all([
+							async function () {
+								prev && prev.removeAttribute("active");
+								item.setAttribute("active", "");
+							}(),
+							FEATURE.setActive(item.id)
+						])
+							.then(
+								ifr.src = `/feature/${item.id}/index.html`
+							)
+
+					}
+
+					async function addFeatureToMenu(menuNode, feat) {
+						var a = top.document.createElement("a")
+						menuNode.appendChild(a),
+							a.classList.add("feature-menu-item-a"),
+							a.target = "feature-iframe",
+							a.id = feat.id,
+							a.onclick = onFeatureMenuItemClick;
+
+						var div = top.document.createElement("div");
+						a.appendChild(div),
+							div.classList.add("feature-menu-item-layout")
+
+						var textLayoutDiv = top.document.createElement("div");
+						div.appendChild(textLayoutDiv),
+							textLayoutDiv.style.display = "inline"
+
+						var textDiv = top.document.createElement("div");
+						textLayoutDiv.appendChild(textDiv),
+							textDiv.classList.add("feature-menu-item-text"),
+							textDiv.innerHTML = feat.name;
+
+						await fetch(platform.runtime.getURL(`/feature/${feat.id}/icon.svg`))
+							.then(r => r.text())
+							.then(t => {
+								div.innerHTML = t + div.innerHTML;
+								return div.querySelector("svg");
+							})
+							.then(
+								svg => {
+									svg.classList.add("feature-menu-item-icon");
+									svg.style.display = "inline";
+								}
+							);
+					}
+				},
+				{ once: true },
+			);
+		}
+	);
